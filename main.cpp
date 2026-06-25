de0be166fd3e29ade8ed21e726a3ea1e280b4a68
akhil jaggarwal <akhil.jaggarwal@gmail.com>
2026-06-25 08:41:53 +0100

    main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,90 @@
-#include "cluster.h"
-#include "serverinfo.h"
+#include "server.h"
 
-#include <memory>
+#include <cerrno>
+#include <cstdlib>
+
+#include <fstream>
+#include <iostream>
+#include <string_view>
 #include <vector>
 
-int main(int argc, char** argv) {
-    // extract server info from args
-    std::vector<ServerInfo> serverInfos;
-    std::unique_ptr<Cluster> pCluster = std::make_unique<Cluster>(std::move(serverInfos));
+#include <unistd.h>
+#include <sys/types.h>
 
-    pCluster->start();
+
+std::string ParseConfigFile(const char* file_name) {
+    std::ifstream ifs{file_name};
+    if (!ifs) {
+        std::cerr << "bad fname\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    std::string hosts;
+    if (!(ifs >> hosts)) {
+        std::cerr << "fname: can't read\n";
+        std::exit(exit(EXIT_FAILURE));
+    }
+    
+    return hosts;
+}
+
+int main(int argc, char** argv) {
+
+    if (argc < 2) {
+        std::cerr << "Usage:\n";
+        std::cerr << "raft-runner <num-servers> <config-file-name>"
+        return 1;    
+    }
+
+    const int num_servers{std::atoi(argv[1])}; 
+
+    std::string hosts{ParseConfigFile(argv[2])};
+
+    static char* newargv[] = {
+        nullptr, 
+        argv[1],
+        nullptr,
+        hosts.data(),
+        nullptr
+    };
+    newargv[0] = "server";
+    static char* newenviron[] = {nullptr};
+
+    std::vector<pid_t> childpids;
+    for (int i{}; i<num_servers; ++i) {
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            perror("fork");
+            std::exit(EXIT_FAILURE);
+        }
+        std::string sidx{std::to_string(i)};
+        newargv[2] = sidx.data();
+        if (pid == 0) {
+            int ret = execve(newargv[0], newargv, newenviron);
+            if (ret == -1) {
+                perror("execve");
+                std::exit(EXIT_FAILURE);
+            }
+        } else {
+            childpids.push_back(pid);
+        }
+    }
+    
+    int wstatus;
+    for (auto pid : childpids) {
+        do {
+            pid_t w = waitpid(pid, &wstatus, WNOHANG);
+            if (w == -1) {
+                perror("waitpid)");
+                std::exit(EXIT_FAILURE);
+            }
+
+            if (WIFEXITED((wstatus))) {
+                std::cerr << "exited status=" << WEXITSTATUS(wstatus) << std::endl;
+            }
+        } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+    }    
 
     return 0;
 }
